0ceca9ff0b6bc5f088e0d408a630ed2a19a045c7
joyAct <joyleeact@gmail.com>
2020-10-24 12:03:26 +0900

    리액트 3주차 수업 : React Hook - useEffect로 상태 업데이트

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import SearchInput from '../components/search/input'
 import RecentSearchList from '../components/search/recent-search-list'
 
 function SearchPage() {
-  const [keywords, setKeywords] = useState([
-    { id: 0, label: '아이유' },
-    { id: 1, label: '금요일에 만나요' },
-  ])
+  const [keywords, setKeywords] = useState([])
+
+  useEffect(() => {
+    setKeywords(JSON.parse(localStorage.getItem('keywords') || '[]'))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('keywords', JSON.stringify(keywords))
+  }, [keywords])
 
   const handleAddKeyword = (keyword) => {
     setKeywords([
